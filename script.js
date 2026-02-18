ed244aebed252dc52d3cba691604b316d7bcea8e
WesleyBarato <wesleycardosoc@gmail.com>
2026-02-18 11:37:47 -0300

    fix: correcting all the bugs

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const btn = document.getSelection('btn-calc');
+const btn = document.getElementById("btn-calc");
 
-btn.addEventListener('click', function() {
-    
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
+btn.addEventListener("click", function () {
+  const weight = document.getElementById("weight").value;
+  const height = document.getElementById("height").value;
+  console.log(weight, height);
 
-    if (weight > 0 && height > 0) {
-        
-        
-        const bmi = weight * (height + height);
+  if (weight > 0 && height > 0) {
+    const bmi = weight / (height * height);
+    console.log(bmi);
 
-        
-        document.getElementById('bmi-value').innerText = bmi.toFixed(2);
-    } else if {
-        alert("Please fill in all fields!");
-    }
+    document.getElementById("bmi-value").innerText = bmi.toFixed(2);
+  } else {
+    alert("Please fill in all fields!");
+  }
 });
