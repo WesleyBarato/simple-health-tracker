fb41d3f8708c33c3706a3e20b7a078177309a327
LucasSilveiradaLuz <luzlucas22022007@gmail.com>
2026-02-18 11:25:22 -0300

    fix: fixing bmi calculation

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ const btn = document.getElementById('btn-calc');
 btn.addEventListener('click', function() {
     
     const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
+    const height = document.getElementById('height').value/100
 
     if (weight > 0 && height > 0) {
-        
-        const bmi = weight * (height + height);
-
+    
+        const bmi = weight / (height * height);
         
         document.getElementById('bmi-value').innerText = bmi.toFixed(2);
+           
     } 
+ 
     else {
         alert ("Please fill in all fields!")
     }
