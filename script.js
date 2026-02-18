c7193018f3adf5b749d1bf3da83b916b247723b0
RafaelRossau <rafaelrossau@gmail.com>
2026-02-18 11:55:20 -0300

    feat: Adicionando status

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ btn.addEventListener('click', function() {
         
         
      const bmi = weight / (height * height);
+     if(bmi < 18.5){
+        document.getElementById('description').innerText = "Abaixo do peso"
+     }
+     else if(bmi >= 18.5 && bmi <= 24.9){
+        document.getElementById('description').innerText = "Saudável"
+
+     }
+     else if(bmi >= 25 && bmi <= 29.9){
+        document.getElementById('description').innerText = "Sobrepeso"
+     }
+     else{
+        document.getElementById('description').innerText = "Obesidade"
+     }
      
     document.getElementById('bmi-value').innerText = bmi.toFixed(2)
     } else {
